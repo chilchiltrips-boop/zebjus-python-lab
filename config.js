f8394ad4c9c9f4d107b54b73e274c613f1e6ce65
chilchiltrips-boop <chilchiltrips@gmail.com>
2026-09-01 13:00:13 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ window.ZEBJUS_CONFIG = {
   websocketUrl: "",
   defaultKitId: "ZB-000123",
   mediaPipe: {
-    wasmRoot: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm",
+    moduleUrl: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.21/+esm",
+    wasmRoot: "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.21/wasm",
     modelUrl: "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task"
   }
 };
